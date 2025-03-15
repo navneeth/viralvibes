7b3194bd93e399ea139a5829c5431075b81b6357
Nav <navneeth.s@gmail.com>
2025-03-15 06:51:55 +0000

    reqs

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 from fasthtml.common import *
 import yt_dlp
 import pandas as pd
-import squarify
-import matplotlib.pyplot as plt
+#import squarify
+#import matplotlib.pyplot as plt
 import io
 import base64
 
 app, rt = fast_app(hdrs=(picolink))
 
+def get_playlist_videos(playlist_url):
+    """Extracts video data from a YouTube playlist."""
+    ydl_opts = {
+        'quiet': True,
+        'extract_flat': True,
+        'force_generic_extractor': True
+    }
+    
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        playlist_info = ydl.extract_info(playlist_url, download=False)
+
+        if 'entries' in playlist_info:
+            videos = playlist_info['entries']
+            data = [{
+                "Rank": rank, 
+                "Title": video.get('title', 'N/A'), 
+                "Views": video.get('view_count', 0),
+                "Uploader": video.get('uploader', 'N/A'),
+                "Channel ID": video.get('channel_id', 'N/A')
+            } for rank, video in enumerate(videos, start=1)]
+            
+            return pd.DataFrame(data)
+    return pd.DataFrame()
 
 @rt("/")
 def get():
